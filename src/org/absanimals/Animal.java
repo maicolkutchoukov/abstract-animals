d3952ad5c7ad70b170c4102c05954ad173bdddba
maicolkutchoukov <maicol.j.95@gmail.com>
2024-05-22 14:37:10 +0200

    Animal Class

---
+++ src/org/absanimals/Animal.java
@@ -0,0 +1,28 @@
+package org.absanimals;
+
+public abstract class Animal {
+    private String name;
+    private String preFood;
+    private static final String[] foods = {"Grass", "Meat", "Milk"};
+    public Animal(String name, String preFood){
+        this.name = name;
+        this.preFood = preFood;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPreFood() {
+        return preFood;
+    }
+
+    public void sleep() {
+        System.out.println("Zzzz");
+    }
+
+    // METODO ASTRATTO
+    public abstract void makeSound();
+
+    public abstract boolean eat(String food) throws IllegalArgumentException;
+}
